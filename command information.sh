4152815a279a9b27e50cb6d443ca85c5d0a1d203
huntedhappy <huntedhappy@gmail.com>
2021-11-23 18:59:33 +0900

    dokyung

--- command information.sh
+++ command information.sh
@@ -7,6 +7,9 @@ kubectl set image deploy nginx nginx=nginx:latest
 # autoscale
 kubectl autoscale deployment php-apache --cpu-percent=50 --min=1 --max=10
 
+# autosclae check
+kubectl get hpa
+
 # label 
 kubectl label deploy nginx test=test
 
@@ -18,3 +21,6 @@ kubectl annotate deploy nginx test=tesst
 
 # annotation check
 kubectl get deploy nginx -o jsonpath={.metadata.annotations}
+
+# pause
+kubectl rollout pause deploy nginx
